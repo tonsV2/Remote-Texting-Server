3cfe0c653da54ee21de026f9c9c6c5403b24acc5
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-08-13 08:57:41 +0200

    Added optional ContactResource to PhoneNumbers.

--- src/main/java/dk/fitfit/remotetexting/api/resource/ContactResource.java
+++ src/main/java/dk/fitfit/remotetexting/api/resource/ContactResource.java
@@ -1,9 +1,7 @@
 package dk.fitfit.remotetexting.api.resource;
 
 
-import dk.fitfit.remotetexting.business.domain.Message;
 import dk.fitfit.remotetexting.business.domain.PhoneNumber;
-import dk.fitfit.remotetexting.business.domain.User;
 import org.springframework.hateoas.ResourceSupport;
 
 import java.util.HashSet;
@@ -11,9 +9,7 @@ import java.util.Set;
 
 public class ContactResource extends ResourceSupport {
 	private String name;
-	private User user;
 	private Set<PhoneNumber> phoneNumbers = new HashSet<>();
-	private Set<Message> messages = new HashSet<>();
 
 	public String getName() {
 		return name;
@@ -23,27 +19,11 @@ public class ContactResource extends ResourceSupport {
 		this.name = name;
 	}
 
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(final User user) {
-		this.user = user;
-	}
-
 	public Set<PhoneNumber> getPhoneNumbers() {
 		return phoneNumbers;
 	}
 
 	public void setPhoneNumbers(final Set<PhoneNumber> phoneNumbers) {
 		this.phoneNumbers = phoneNumbers;
-	}
-
-	public Set<Message> getMessages() {
-		return messages;
-	}
-
-	public void setMessages(final Set<Message> messages) {
-		this.messages = messages;
 	}
 }
