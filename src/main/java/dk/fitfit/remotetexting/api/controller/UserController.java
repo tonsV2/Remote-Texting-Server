628922d092b309ad0d0ea15f4ced6792c0fce0db
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-08-17 17:27:35 +0200

    s/CurrentUser/CurrentUserHolder/ Using CurrentUserHolder in MessageController. Made sent and received methods of MessageController return http status NOT_IMPLEMENTED.

--- src/main/java/dk/fitfit/remotetexting/api/controller/UserController.java
+++ src/main/java/dk/fitfit/remotetexting/api/controller/UserController.java
@@ -3,9 +3,7 @@ package dk.fitfit.remotetexting.api.controller;
 import dk.fitfit.remotetexting.api.resource.UserResource;
 import dk.fitfit.remotetexting.api.resource.assembler.UserResourceAssembler;
 import dk.fitfit.remotetexting.business.domain.User;
-import dk.fitfit.remotetexting.util.CurrentUser;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import dk.fitfit.remotetexting.util.CurrentUserHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,18 +14,15 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RestController
 @RequestMapping("/api")
 public class UserController {
-	private final Logger log = LoggerFactory.getLogger(this.getClass());
-
 	@Autowired
-	private CurrentUser currentUser;
+	private CurrentUserHolder currentUserHolder;
 
 	@Autowired
 	private UserResourceAssembler userResourceAssembler;
 
 	@RequestMapping(value = "/users", method = GET)
 	public UserResource getUser() {
-		User user = currentUser.getUser();
+		User user = currentUserHolder.getUser();
 		return userResourceAssembler.toResource(user);
 	}
-
 }
