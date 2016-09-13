dbd30dbbbdc152c2d256765c7f3510c85fc16ef5
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-09-13 18:00:23 +0200

    Added HibernateJpaSessionFactoryBean.

--- src/main/java/dk/fitfit/remotetexting/business/PersistenceConfig.java
+++ src/main/java/dk/fitfit/remotetexting/business/PersistenceConfig.java
@@ -1,10 +1,21 @@
 package dk.fitfit.remotetexting.business;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+
+import javax.persistence.EntityManagerFactory;
 
 
 @Configuration
 @EnableJpaRepositories
 public class PersistenceConfig {
+
+	@Bean
+	public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+		HibernateJpaSessionFactoryBean factory = new HibernateJpaSessionFactoryBean();
+		factory.setEntityManagerFactory(emf);
+		return factory;
+	}
 }
